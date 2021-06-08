139d803b544a541e67cd134a6fd044dfdf5cbe34
Sensokaku <28125632+Sensokaku@users.noreply.github.com>
2021-06-08 13:22:07 +0800

    change more api tings

--- scripts/downloader.js
+++ scripts/downloader.js
@@ -42,7 +42,7 @@ function startpreview(box) {
 }
 
 function log_to_server(message) {
-    let url = "http://api.osugame.online/log/?msg=" + message;
+    let url = "https://api.kaku.moe/log/?msg=" + message;
     let xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.send();
