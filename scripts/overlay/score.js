73357aa1534fba5677e65249336d78accbff1ad0
Sensokaku <28125632+Sensokaku@users.noreply.github.com>
2021-06-08 13:21:25 +0800

    change api tings

--- scripts/overlay/score.js
+++ scripts/overlay/score.js
@@ -251,7 +251,7 @@ define([], function()
 
         function uploadScore(summary) {
             let xhr = new XMLHttpRequest();
-            let url = "http://api.osugame.online/send/";
+            let url = "https://api.kaku.moe/send/";
             url += "?sid=" + encodeURIComponent(summary.sid);
             url += "&bid=" + encodeURIComponent(summary.bid);
             url += "&title=" + encodeURIComponent(summary.title);
